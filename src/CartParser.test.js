3bc383d9354883fa50d1fe982f64add77a1a2a93
Saoa35 <saoamah@gmail.com>
2023-08-09 15:24:59 +0400

    + items and total price

--- src/CartParser.test.js
+++ src/CartParser.test.js
@@ -1,15 +1,26 @@
-import CartParser from './CartParser';
+import CartParser from "./CartParser";
 
 let parser;
 
 beforeEach(() => {
-	parser = new CartParser();
+  parser = new CartParser();
 });
 
-describe('CartParser - unit tests', () => {
-	// Add your unit tests here.
+describe("CartParser - unit tests", () => {
+  // Add your unit tests here.
+  describe("parse", () => {
+    it("should return an object with items and total price", () => {
+      const result = parser.parse("./samples/cart.csv");
+      expect(result).toHaveProperty("items");
+      expect(result).toHaveProperty("total");
+    });
+
+    it("should throw an Error, if the invalid file was passed", () => {
+      expect(() => parser.parse("./samples/cart.json")).toThrow();
+    });
+  });
 });
 
-describe('CartParser - integration test', () => {
-	// Add your integration test here.
+describe("CartParser - integration test", () => {
+  // Add your integration test here.
 });
